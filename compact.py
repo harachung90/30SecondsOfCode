2caa8ae85ec7f2e1b930c3ac5553230e6aba87ec
Hara Chung <harachung90@gmail.com>
2022-09-27 20:18:43 +1000

    Fourth commit. compact()

---
+++ compact.py
@@ -0,0 +1,7 @@
+def compact(lst):
+    return list(filter(None, lst))
+
+
+compact([0, 1, False, 2, '', 3, 'a', 's', 34])  # [1,2, 3, 'a', 's', 34]
+
+print(compact([0, 1, False, 2, '', 3, 'a', 's', 34]))
