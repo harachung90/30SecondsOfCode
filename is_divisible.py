2174ca4162f99df53c746a217c92d00e845cb766
Hara Chung <harachung90@gmail.com>
2022-10-08 22:40:33 +1100

    Twenty-fourth commit. is_divisible()

---
+++ is_divisible.py
@@ -0,0 +1,9 @@
+def is_divisible(dividend, divisor):
+    return dividend % divisor == 0
+
+
+is_divisible(6, 3) # True
+is_divisible(4, 3) # False
+
+print(is_divisible(6, 3))
+print(is_divisible(4, 3))
