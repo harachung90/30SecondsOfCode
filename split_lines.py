1f7966efbad5765a317b37e40fa2c230bc5e69cc
Hara Chung <harachung90@gmail.com>
2022-10-17 18:53:41 +1100

    Forty-second commit. similarity()

---
+++ split_lines.py
@@ -0,0 +1,7 @@
+def split_lines(s):
+    return s.split('\n')
+
+
+split_lines('This\nis\na\nmultiline\nstring.') #['This', 'is', 'a', 'multiline', 'string.']
+
+print(split_lines('This\nis\na\nmultiline\nstring.'))
